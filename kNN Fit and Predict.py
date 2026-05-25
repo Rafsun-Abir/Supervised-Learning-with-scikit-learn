65154618c3a8592e6cfb6612b1d6bf506b764673
Rafsun-Abir <abir0340@gmail.com>
2026-05-25 14:49:29 +0600

    kNN Fit and Predict.py

---
+++ kNN Fit and Predict.py
@@ -0,0 +1,22 @@
+# Import KNeighborsClassifier
+import pandas as pd
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+churn_df = pd.read_csv(r"D:\DataCamp_ADS\Supervised Learning with scikit-learn\telecom_churn_clean.csv")
+y = churn_df["churn"].values
+X = churn_df[["account_length", "customer_service_calls"]].values
+
+# Create a KNN classifier with 6 neighbors
+knn = KNeighborsClassifier(n_neighbors=6)
+
+# Fit the classifier to the data
+knn.fit(X, y)
+
+# Predict the labels for the X_new
+X_new = np.array([[30.0, 17.5],
+                  [107.0, 24.1],
+                  [213.0, 10.9]])
+y_pred = knn.predict(X_new)
+
+# Print the predictions
+print("Predictions: {}".format(y_pred)) 
